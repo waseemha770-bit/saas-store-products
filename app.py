1737e17c5caf6114fcda0009bc85feb86e393ba1
waseem hamdan <waseemha770@gmail.comgit>
2026-08-23 14:55:22 +0300

    Fix: Removed crashing Flask-Session & implemented strict product sanitization to bypass Jinja groupby errors.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, Response, abort
 import database, os, urllib.parse, io, csv, json, urllib.request, urllib.error
 from datetime import datetime, timedelta
-from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
@@ -29,20 +28,7 @@ csrf.exempt("apply_coupon")
 csrf.exempt("checkout")
 
 # إعداد الجلسات المخزنة في MongoDB لتجنب مشاكل Vercel Serverless
-app.config['SESSION_TYPE'] = 'mongodb'
 
-# تأمين جلب عميل قاعدة البيانات
-try:
-    app.config['SESSION_MONGODB'] = database.client
-except AttributeError:
-    # تعطيل جلسات MongoDB والعودة للجلسات المحلية إذا فشل الاتصال لحماية السيرفر
-    app.config['SESSION_TYPE'] = 'filesystem'
-
-app.config['SESSION_MONGODB_DB'] = 'tajergo_db'
-app.config['SESSION_MONGODB_COLLECT'] = 'sessions'
-app.config['SESSION_PERMANENT'] = True
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
-Session(app)
 
 MAIN_DOMAIN = "saas-store-products.vercel.app"
 
